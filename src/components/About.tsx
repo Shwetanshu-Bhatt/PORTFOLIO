329046b2a357b3f0e3dd99ef365337d26b5e3912
Shwetanshu-Bhatt <shwetanshubhatt@.com>
2025-03-18 08:44:58 +0530

    theme changes improvements

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ export default function About() {
       <section id="about" className="theme py-20 px-5 text-center">
         <h2 className="text-4xl font-bold">About Me</h2>
         <p className="mt-5 pb-6 text-lg max-w-3xl mx-auto">
-          I&apos;ssionate B.Tech student with a focus on AI, ML, and software development.  
+          I&apos;am a Passionate B.Tech student with a focus on AI, ML, and software development.  
           I love working on innovative projects, from financial applications to AI-driven solutions.
         </p>
       </section>
